dc8e4e055916a56060ef387a7d3f65814ede6b46
nikolouze93 <nikaberidze27@gmail.com>
2026-03-30 21:09:12 +0400

    Add initial implementation of RSS parser with JSON support

---
+++ main.py
@@ -0,0 +1,42 @@
+from argparse import ArgumentParser
+from typing import List, Optional, Sequence
+import requests
+
+
+class UnhandledException(Exception):
+    pass
+
+
+def rss_parser(
+    xml: str,
+    limit: Optional[int] = None,
+    json: bool = False,
+) -> List[str]:
+    """
+    parses RSS XML and returns formatted strings or a JSON-ready list.
+    """
+
+def main(argv: Optional[Sequence] = None):
+    parser = ArgumentParser(
+        prog="PyStream",
+        description="high-performance, standalone RSS parser with JSON serialization support",
+    )
+    parser.add_argument("source", help="RSS URL", type=str, nargs="?")
+    parser.add_argument(
+        "--json", help="Print result as JSON in stdout", action="store_true"
+    )
+    parser.add_argument(
+        "--limit", help="Limit news topics if this parameter provided", type=int
+    )
+
+    args = parser.parse_args(argv)
+    xml = requests.get(args.source).text
+    try:
+        print("\n".join(rss_parser(xml, args.limit, args.json)))
+        return 0
+    except Exception as e:
+        raise UnhandledException(e)
+
+
+if __name__ == "__main__":
+    main()
